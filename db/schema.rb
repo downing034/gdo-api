ac238c68dfd30d34f28dc79d03496b88b3c5a49c
Paul Downing <downing034@gmail.com>
2025-09-04 11:26:15 -0600

    feat: add sources seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_28_195413) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_04_172444) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
+
+  create_table "data_sources", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "code"
+    t.string "name"
+    t.string "base_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "leagues", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.uuid "sport_id", null: false
--- db/seeds/leagues.rb
+++ db/seeds/leagues.rb
@@ -7,7 +7,7 @@ leagues = [
   { sport: baseball, code: 'mlb', name: 'Major League Baseball', display_name: 'MLB', has_conferences: true, active: true },
   { sport: basketball, code: 'ncaam', name: 'NCAA Men\'s Basketball', display_name: 'NCAAM', has_conferences: true, active: true },
   { sport: football, code: 'ncaaf', name: 'NCAA Football', display_name: 'NCAAF', has_conferences: true, active: true },
-  { sport: football, code: 'nfl', name: 'National Football League', display_name: 'NFL', has_conferences: true, active: false },
+  { sport: football, code: 'nfl', name: 'National Football League', display_name: 'NFL', has_conferences: true, active: true },
   { sport: basketball, code: 'nba', name: 'National Basketball Association', display_name: 'NBA', has_conferences: true, active: false },
   { sport: hockey, code: 'nhl', name: 'National Hockey League', display_name: 'NHL', has_conferences: true, active: false }
 ]
