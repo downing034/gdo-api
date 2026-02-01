c9756bdcbf952f3e113d64b733b4662688205502
Paul Downing <downing034@gmail.com>
2026-02-01 00:25:58 -0700

    feat: complete initial api endpoints

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,20 @@ Rails.application.configure do
 
   # Log to STDOUT with the current request id as a default log tag.
   config.log_tags = [ :request_id ]
-  config.logger   = ActiveSupport::TaggedLogging.logger(STDOUT)
+  config.log_formatter = ::Logger::Formatter.new
+
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = proc do |severity, timestamp, progname, msg|
+      {
+        timestamp: timestamp.iso8601,
+        level: severity,
+        request_id: progname,
+        message: msg
+      }.to_json + "\n"
+    end
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end)
 
   # Change to "debug" to log everything (including potentially personally-identifiable information!)
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
